3b466305bbda7189fbaa22414203af534ecfdb5b
Heytor Victor <heytorvas@gmail.com>
2021-03-04 16:21:01 -0300

    end day 03

---
+++ expenses/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:expenses/components/transaction_form.dart';
+import 'package:flutter/material.dart';
+
+import 'dart:math';
+import './components/transaction_form.dart';
+import './components/transaction_list.dart';
+import 'models/transaction.dart';
+
+main() => runApp(ExpensesApp());
+
+class ExpensesApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage()
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  final _transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now()
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now()
+    )
+  ];
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transaction(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+     _transactions.add(newTransaction);
+    });
+  }
+
+  _openTransactoinFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context, 
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      }
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _openTransactoinFormModal(context),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                child: Text('Gráfico')
+              ),
+            ),
+            TransactionList(_transactions),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _openTransactoinFormModal(context),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
